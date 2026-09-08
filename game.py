bb7c951ba6abe5cca561b2aa6690e9b96675dd3e
francescocalabrese2005-spec <francesco.calabrese.2005@gmail.com>
2026-09-08 19:01:09 +0200

    metodo move

--- game.py
+++ game.py
@@ -1,13 +1,13 @@
 import random
-
+from table import _Table
 class Game:
     def __init__(self, width:int, heigth:int) -> None:
         self._width = width
         self._height = heigth
-        self._cv : tuple[int,int] = (random.randint(1,width),random.randint(1,heigth)) #ricorda di cambiare
+        t = _Table(self)
         self._completed : bool = False
-        self._table : list[list[int]] = [[0] * width for _ in range (heigth)]
-       
+        self._table : list[list[int]] = t.tabellone
+        self._cv : tuple[int,int] = t.cv 
         
 
     @property
@@ -25,6 +25,39 @@ class Game:
     @property
     def table(self):
         return self._table
-    
+
+    @property
+    def cv(self):
+        return self._cv
+
+    def move(self, row:int, column:int) -> None:
+        if self.completed or (row, column) != self.cv or (self.cv[0] != row and self.cv[1] != column):
+            return
+        tmp : list[int] = []
+        if row == self.cv[0]:
+            tmp  , scelta= (self.table[row][self.cv[1]+1 : column], True) if column > self.cv[1] else (self.table[row][column : self.cv[1] - 1],False)
+            if scelta:
+                for i in range(abs(row-self.cv[0])):
+                    self.table[row][self.cv[1]+i] = tmp[i]
+                self.table[row][column] = 0
+            else:
+                for i in range(abs(row-self.cv[0])):
+                    self.table[row][self.cv[1]-i] = tmp[i]
+                self.table[row][column] = 0
+        else:
+            tmp, scelta = (
+            ([self.table[i][column] for i in range(self.cv[0] + 1, row + 1)], True)
+            if row > self.cv[0]
+            else ([self.table[i][column] for i in range(row, self.cv[0])], False)
+            )
+            if scelta:
+                for i in range(abs(column-self.cv[1])):
+                    self.table[self.cv[0]+i][column] = tmp[i]
+                self.table[row][column] = 0
+            else:
+                for i in range(abs(column-self.cv[1])):
+                    self.table[self.cv[0]-i][column] = tmp[i]
+                self.table[row][column] = 0
+        return    
 
         
